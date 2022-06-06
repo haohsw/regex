96151c2c698f4b7eed30e9d050911cb4f63c07c8
haohsw <103911006+haohsw@users.noreply.github.com>
2022-06-06 11:48:02 +0700

    init

--- regex.py
+++ regex.py
@@ -5,3 +5,15 @@ regex = r"^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)"
 
 # Japanese characters
 regex = r"([\p{Hiragana}\p{Katakana}\p{Han}\s]+)"
+
+
+\d*
+# any number of digits
+
+\.
+# a dot
+
+\d*
+# more digits.
+\.?000 
+# .000
